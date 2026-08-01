dfc66b2078b9d48bfd8d84945dc10bc730dc573e
Romeo Sarkar <romeosarkar10x@gmail.com>
2026-08-01 12:42:38 +0530

    Use page metadata

--- build.ts
+++ build.ts
@@ -2,6 +2,8 @@ import path from "node:path";
 import fs from "node:fs";
 import esbuild from "esbuild";
 import { copy } from "esbuild-plugin-copy";
+import z from "zod";
+import { PageMetadataSchema } from "./src/schemas/page-metadata.ts";
 
 const SCRIPT_DIR = import.meta.dirname;
 const OUT_DIR = "dist";
@@ -43,16 +45,27 @@ function getRouteName(entryPoint: string) {
 
     return route + "/";
 }
-const entryPoints = contents.filter((v) => v.endsWith("index.tsx"));
-const routes = entryPoints.map(getRouteName);
 
-entryPoints.forEach((entryPoint) => {
-    const directory = path.dirname(entryPoint);
-    const fileName = path.basename(entryPoint).slice(0, -4);
+const entryPoints = contents.filter((v) => v.endsWith(path.sep + "index.tsx"));
+
+const pages = await Promise.all(
+    entryPoints.map(async (entryPoint) => {
+        const importPath = "./" + path.join(DIR, entryPoint.slice(0, -9) + "metadata.ts");
+        // console.log("importPath:", importPath);
+        const { metadata } = z.object({ metadata: PageMetadataSchema }).parse(await import(importPath));
+        return { pathname: entryPoint, route: getRouteName(entryPoint), metadata };
+    }),
+);
+
+// const routes = entryPoints.map(getRouteName);
+
+pages.forEach(({ pathname }) => {
+    const directory = path.dirname(pathname);
+    const fileName = path.basename(pathname).slice(0, -4);
     const outputDirectory = path.join(OUT_DIR, directory);
 
     esbuild.build({
-        entryPoints: [path.join(DIR, entryPoint)],
+        entryPoints: [path.join(DIR, pathname)],
         bundle: true,
         minify: true,
         jsx: "automatic",
@@ -61,8 +74,8 @@ entryPoints.forEach((entryPoint) => {
         target: ["es2022"],
         outdir: outputDirectory,
         define: {
-            __ROUTES: JSON.stringify(routes),
-            __CURRENT_ROUTE: JSON.stringify(getRouteName(entryPoint)),
+            __PAGES: JSON.stringify(pages),
+            __CURRENT_ROUTE: JSON.stringify(getRouteName(pathname)),
         },
     });
 

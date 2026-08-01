ad2d007751d2eb5b061f9c63c85b963e1d228003
Romeo Sarkar <romeosarkar10x@gmail.com>
2026-08-01 23:23:50 +0530

    Auto-detect github CI, and set respective baseUrl

--- scripts/config.ts
+++ scripts/config.ts
@@ -1,6 +1,17 @@
+function getBaseUrl(defaultBaseUrl: string): string {
+    const githubRepository = process.env.GITHUB_REPOSITORY;
+
+    if (githubRepository === undefined) {
+        return defaultBaseUrl;
+    }
+
+    const [, repo] = githubRepository.split("/");
+    return `/${repo}`;
+}
+
 export const BUILD_CONFIG = {
     DIR: "src/pages",
     OUT_DIR: "dist",
     TYPEGEN_OUT_FILE: "src/generated/types.ts",
-    BASE_URL: "project/pagelab",
+    BASE_URL: getBaseUrl("/"),
 };
